560ff7507e03070f47f60895dfe3e02e09fd531b
klaus <oriokiklaus@gmail.com>
2024-03-09 01:42:24 +0300

    change button id so it registers on event listener and collects joke on click

--- js/joke.js
+++ js/joke.js
@@ -1,11 +1,29 @@
+// document.addEventListener("click", function (event) {
+//     // Checking if the button was clicked
+//     if (!event.target.matches("#button")) return;
+  
+//     fetch("https://official-joke-api.appspot.com/random_joke")
+//       .then((response) => response.json())
+//       .then((data) => renderJoke(data));
+//   });
+
+//   postToServerWithAjax('/invite_preview', null, function (response) {
+//     var wind = window.open("", "popupWindow", "width=600,height=600,scrollbars=yes");
+//     wind.document.write(response);
+//  });
+
 document.addEventListener("click", function (event) {
     // Checking if the button was clicked
     if (!event.target.matches("#button")) return;
   
+    // let data = {"type":"general","setup":"What do you call a pile of cats?","punchline":" A Meowtain.","id":217};
     fetch("https://official-joke-api.appspot.com/random_joke")
       .then((response) => response.json())
       .then((data) => renderJoke(data));
+    // Log the static data
+    // alert(JSON.stringify(data, null, 2));
   });
+  
   
   function renderJoke(data) {
     const setup = document.getElementById("setup");
